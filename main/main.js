00eca1706560e13d4d020688d182d5da10f8f540
jessie-gan <jessiegan@foxmail.com>
2018-04-21 21:59:41 +0800

    guess number right or wrong

--- main/main.js
+++ main/main.js
@@ -1,6 +1,40 @@
 // Write your cade below:
 function main() {};
-
-module.exports = {
-    main
+function generateNumber(){
+    let initNum = [];
+    let book = [];
+    for(let i=0;i<=10;i++){
+        book.push(0);
+    }
+    for(let i=0;i<4;i++ ){
+        let temp = Math.ceil(Math.random()*10);
+        while(temp==10){
+        }
+        while(book[temp]){
+            temp = Math.ceil(Math.random()*10);
+            if(temp==10){
+                temp = Math.ceil(Math.random()*10);
+            }
+        }
+        book[temp]=1;
+        initNum.push(temp);
+    }
+    console.log(initNum);
+    return initNum;
+}
+//generateNumber();
+module.exports = function judgeNum(initNum,answerNum){
+    let ANum=0,BNum=0;
+    let result;
+    for(let i=0;i<4;i++){
+        if(initNum.indexOf(answerNum[i])!=-1){
+            BNum++;
+        }
+        if(answerNum[i]==initNum[i]){
+            ANum++;
+            BNum--;
+        }
+    }
+    result = ANum+'A'+BNum+'B';
+    return result;
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,28 @@
-const main = require('../main/main');
+const judgeGame = require('../main/main');
 
 describe('main()', () => {
-
+    //var initAnswer = main.generateNumber();
+    var initNum;
+    var answerNum;
+    it('locations all right',function(){
+        initNum=[2,5,1,6];
+        answerNum=[2,5,1,6];
+        var expected = '4A0B';
+        var actual = judgeGame(initNum,answerNum);
+        expect(actual).toEqual(expected);
+    });
+    it('some locations are right',function(){
+        initNum=[2,5,1,6];
+        answerNum=[2,1,5,6];
+        var expected = '2A2B';
+        var actual = judgeGame(initNum,answerNum);
+        expect(actual).toEqual(expected);
+    });
+    it('no matches',function(){
+        initNum=[2,5,1,6];
+        answerNum=[3,7,8,9];
+        var expected = '0A0B';
+        var actual = judgeGame(initNum,answerNum);
+        expect(actual).toEqual(expected);
+    });
 });
